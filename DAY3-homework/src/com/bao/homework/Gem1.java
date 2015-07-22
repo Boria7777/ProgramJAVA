d1df7ed2c6c784ae3152dc748621ebcd1b416090
Boria <boria7777@outlook.com>
2015-07-22 15:20:19 +0800

    baocun

--- DAY3-homework/src/com/bao/homework/Gem1.java
+++ DAY3-homework/src/com/bao/homework/Gem1.java
@@ -4,7 +4,17 @@ public class Gem1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
+		int [][]a=new int[20][5];
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 5; j++){
+				a[i][j]=(int) (Math.random()*100);
+				System.out.print(a[i][j]+" ");
+			}
+			
+				
+				
+			}
+		}
 }
+
+
